68d0afd795b2ae5d1c1e922b1cc4a392ce4e4ca3
Toru45 <faiqradiansyah@gmail.com>
2025-06-27 12:59:41 +0700

    initial commit

---
+++ OpenShop_Django_RestApi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for OpenShop_Django_RestApi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenShop_Django_RestApi.settings')
+
+application = get_asgi_application()
