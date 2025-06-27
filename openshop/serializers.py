1387a817acca4203128a0b41cc7565b4f6b453b0
Toru45 <faiqradiansyah@gmail.com>
2025-06-27 14:49:17 +0700

    feat _links HATEOAS

--- openshop/serializers.py
+++ openshop/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from openshop.models import OpenShop
+from rest_framework.reverse import reverse
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    _links = serializers.SerializerMethodField()
     class Meta:
         model = OpenShop
         fields = [
@@ -18,4 +20,21 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
             'is_available',
             'picture',
             'is_delete',
+            '_links',
+        ]
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return [
+            {
+                "rel": "self",
+                "href": reverse('product-list', request=request),
+                "action": "POST",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": reverse('product-detail', kwargs={'pk': obj.pk}, request=request),
+                "action": "GET",
+                "types": ["application/json"]
+            }
         ]
