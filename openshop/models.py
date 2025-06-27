923e1a5e1ed08f93638674a22122f98cd91f2ce9
Toru45 <faiqradiansyah@gmail.com>
2025-06-27 13:20:42 +0700

    feat models resource

--- openshop/models.py
+++ openshop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Note(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=30)
+    sku = models.TextField()
+    description = models.TextField()
+    shop = models.TextField()
+    location = models.TextField()
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    category = models.TextField()
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    picture = models.TextField()
+    is_delete = models.BooleanField(default=False)
