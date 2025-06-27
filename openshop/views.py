32b044ddbaf3c4b5f6fa9649b591579653f3d817
Toru45 <faiqradiansyah@gmail.com>
2025-06-27 14:33:30 +0700

    feat implement get endpoint

--- openshop/views.py
+++ openshop/views.py
@@ -6,8 +6,15 @@ from .models import OpenShop
 
 class ProductList(APIView):
     def post(self, request):
-        note = ProductSerializer(data=request.data, context={'request': request})  # Pass request context
+        note = ProductSerializer(data=request.data, context={'request': request}) 
         if note.is_valid(raise_exception=True):
             note.save()
             return Response(note.data, status=status.HTTP_201_CREATED)
         return Response(note.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        notes = OpenShop.objects.all()
+        serializer = ProductSerializer(notes, many=True, context={'request': request})  
+        return Response({
+            "products": serializer.data
+        }, status=status.HTTP_200_OK)
